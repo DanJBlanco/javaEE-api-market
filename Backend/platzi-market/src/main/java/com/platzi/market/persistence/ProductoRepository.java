2ce080accdcef878938f2bdee8bdbff56aa717f1
DJBLANCOH <djblancoh@gmail.com>
2020-10-21 21:05:13 -0500

    anotacion @repository, diferencia con @Component

--- Backend/platzi-market/src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ Backend/platzi-market/src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository _productoCrudRepository;
@@ -21,4 +23,17 @@ public class ProductoRepository {
     public Optional<List<Producto>> getEscasos(int cantidad) {
         return _productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, false);
     }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return _productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return _productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        _productoCrudRepository.deleteById(idProducto);
+    }
+
 }
